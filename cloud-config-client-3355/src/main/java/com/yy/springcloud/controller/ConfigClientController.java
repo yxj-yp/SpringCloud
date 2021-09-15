5f4e39d183a94e37e2c29945fb4744cd47046fcb
yxj-yp <yanhappyj@foxmail.com>
2021-09-15 20:47:27 +0800

    + springcloud config 自动刷新

--- cloud-config-client-3355/src/main/java/com/yy/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/yy/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.yy.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  *Version     : 
  ***/
 @RestController
+@RefreshScope//自动刷新
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
